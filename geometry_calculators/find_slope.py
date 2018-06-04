4f61a88ef9fa4ab00bdd229479af641b3e01b5fe
Huize Mao Wei <S22145>
2018-06-04 16:20:59 +0200

    t

---
+++ geometry_calculators/find_slope.py
@@ -0,0 +1,7 @@
+x1 = float(input("what is point x1: "))
+y1 = float(input("what is point y1: "))
+x2 = float(input("what is point x2: "))
+y2 = float(input("what is point y2: "))
+slope2 = (y1 - y2)/(x1 - x2)
+slope = float(slope2)
+print ("The slope these two points make is {}".format(slope))
