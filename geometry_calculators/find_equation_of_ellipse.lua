5809ba8f0aea73102894d0b5437aa053e922bd56
Huize Mao Wei <S22145>
2018-06-05 13:59:38 +0200

    not completed

---
+++ geometry_calculators/find_equation_of_ellipse.lua
@@ -0,0 +1,8 @@
+io.write("what is the focal radii: ")
+focal_radii = io.read("*n")
+a = focal_radii / 2 
+io.write("what is the x axis of the cener of the ellipse: ")
+x_axis_of_center = io.read("*n")
+io.write("what is the y axis of the cener of the ellipse: ")
+y_axis_of_center = io.read("*n")
+io.write("Is the focus on x-axis or y-axis: ")
