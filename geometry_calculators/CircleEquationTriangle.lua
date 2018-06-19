f31303da5950e304935efab6251bd02f8e41e92a
Huize Mao Wei <S22145>
2018-06-19 13:38:52 +0200

    d

---
+++ geometry_calculators/CircleEquationTriangle.lua
@@ -0,0 +1,6 @@
+io.write("what is one point given that's on the circle?")
+pt1 = io.read("*n")
+io.write("what is the second point given that's on the circle? ")
+pt2 = io.read("*n")
+io.write("what is the last point given that's on the circle? ")
+pt3 = io.read("*n")
